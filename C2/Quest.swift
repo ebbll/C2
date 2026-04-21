c435861d7e739de4a101e2ad38777620bfb5f66f
ebbll <eklee2057@naver.com>
2026-04-21 19:52:50 +0900

    feat: 퀘스트 모델에 카테고리 속성 추가

--- C2/Quest.swift
+++ C2/Quest.swift
@@ -8,9 +8,15 @@
 import SwiftData
 import Foundation
 
+enum QuestCategory: String, Codable, CaseIterable {
+    case academy = "Academy"
+    case postech = "POSTECH"
+    case tool = "Tool"
+}
+
 @Model
 class Quest {
-    @Attribute(.unique) var id: UUID
+    var id: UUID
     
     // 기본 정보
     var title: String
@@ -19,6 +25,7 @@ class Quest {
         steps.count
     }
     var startDate: Date
+    var category: QuestCategory?
     
     // 알림 관련
     var isNotificationEnabled: Bool
@@ -31,6 +38,7 @@ class Quest {
         title: String,
         steps: [String] = [],
         startDate: Date = Date(),
+        category: QuestCategory? = nil,
         isNotificationEnabled: Bool = false,
         notificationTime: Date? = nil,
         notificationID: String? = nil
@@ -39,6 +47,7 @@ class Quest {
         self.title = title
         self.steps = steps
         self.startDate = startDate
+        self.category = category
         self.isNotificationEnabled = isNotificationEnabled
         self.notificationTime = notificationTime
         self.notificationID = notificationID
